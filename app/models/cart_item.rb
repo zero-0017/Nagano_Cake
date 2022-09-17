5ca3c0c5ad2756ad76272d50cea85f4b8b43255a
DanMiyakawa <110312863+DanMiyakawa@users.noreply.github.com>
2022-09-17 11:05:52 +0900

    Revert "Admin item"

--- app/models/cart_item.rb
+++
@@ -1,4 +0,0 @@
-class CartItem < ApplicationRecord
-  belongs_to :customer
-  belongs_to :item
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,3 @@
 Rails.application.routes.draw do
-
-# 顧客用
-# URL /customers/sign_in ...
-devise_for :customers,skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-}
-
-# 管理者用
-# URL /admin/sign_in ...
-devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-}
-
-  namespace :admin do
-    resources :items, only: [:show, :index, :new, :edit, :create, :update]
-  end
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
