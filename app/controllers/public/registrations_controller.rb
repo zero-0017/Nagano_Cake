f592318360c2dde90c76a72d823a0eeb3f680b1e
“DanMiyakawa” <110312863+DanMiyakawa@users.noreply.github.com>
2022-09-18 04:19:37 +0000

    public-signin 修正

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -59,4 +59,8 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+
+  def after_sign_up_path_for(resource)
+    root_url
+  end
 end
