33d727c2c5a3b60b80f6f67580ef60901cbef495
“DanMiyakawa” <110312863+DanMiyakawa@users.noreply.github.com>
2022-09-19 02:08:34 +0000

    test中

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -28,4 +28,9 @@ class Admin::SessionsController < Devise::SessionsController
   def after_sign_in_path_for(resource)
     admin_path
   end
+  
+  def after_sign_out_path_for(resource_or_scope)
+    new_admin_session_path
+  end
+
 end
--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -61,6 +61,6 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # end
 
   def after_sign_up_path_for(resource)
-    root_path
+    customers_my_page_path
   end
 end
