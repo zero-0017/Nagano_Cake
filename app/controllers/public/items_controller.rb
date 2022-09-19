ada8a087982b598af84028f8d118b7a921a30650
“DanMiyakawa” <110312863+DanMiyakawa@users.noreply.github.com>
2022-09-19 06:33:47 +0000

    genre検索機能

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,11 +1,13 @@
 class Public::ItemsController < ApplicationController
   def index
     @items = Item.page(params[:page])
+    @genres = Genre.all
   end
 
   def show
     @item = Item.find(params[:id])
     @cart_item = CartItem.new
+    @genres = Genre.all
   end
 
   private
