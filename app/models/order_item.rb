df315881c689d2d86397c0416782ec65639434f1
“DanMiyakawa” <110312863+DanMiyakawa@users.noreply.github.com>
2022-09-21 03:47:22 +0000

    製作ステータス　制→製

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,7 +2,7 @@ class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :item
 
-  enum production_status: {着手不可:0, 制作待ち:1, 制作中:2, 制作完了:3}
+  enum production_status: {着手不可:0, 製作待ち:1, 製作中:2, 製作完了:3}
 
   def subtotal
     purchase_price * quantity
